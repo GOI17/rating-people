fb237f73332e57d5c4446a0a4eec10c5dc226dde
gilberto-olivas <jose.olivas@abafinance.com.mx>
2020-02-17 14:02:16 -0800

    config express server

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const path = require("path");
+import express, { static } from "express";
+import { join } from "path";
 
 const app = express();
 
-app.use(express.static(`${__dirname}/dist/rating-people`));
+app.use(static(`${__dirname}/dist/rating-people`));
 
 app.get("/*", (req, res) =>
-  res.sendFile(path.join(`${__dirname}/dist/rating-people/index.html`))
+  res.sendFile(join(`${__dirname}/dist/rating-people/index.html`))
 );
 
 app.listen(process.env.PORT || 8080);
